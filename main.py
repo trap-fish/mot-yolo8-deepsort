6bac31f7008b23ab870445850f920cb5304c6f31
trap-fish <fishfoshfish@outlook.com>
2024-11-21 16:39:45 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import cv2
+from ultralytics import YOLO
+import random
+from tracker import Tracker
+
+video_path = os.path.join('.', 'data/okutama', 'okutama-sample.mov')
+video_out_path = os.path.join('.', 'data/okutama', 'okutama-tracked.mp4')
+
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+
+cap_out = cv2.VideoWriter(video_out_path, 
+                          cv2.VideoWriter_fourcc(*'MP4V'), 
+                          cap.get(cv2.CAP_PROP_FPS),
+                          (frame.shape[1], frame.shape[0])
+                          )
+
+# define the detection model
+model = YOLO("yolov8n.pt")
+
+# initialise Tracker object
+tracker = Tracker()
+
+# define colours for bounding boxes
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
+
+while ret:
+
+    results = model(frame)
+
+    for result in results:
+        detections = []
+        for res in result.boxes.data.tolist():
+            x1, y1, x2, y2, score, classid = res
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            classid = int(classid)
+            detections.append([x1, y1, x2, y2, score])
+
+        tracker.update(frame, detections)
+
+        for track in tracker.tracks:
+            bbox = track.bbox
+            x1, y1, x2, y2 = bbox
+            track_id = track.track_id
+
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
+
+
+    # cv2.imshow('frame', frame)
+    # cv2.waitKey(25)
+    cap_out.write(frame)
+    ret, frame = cap.read()
+
+cap.release() # release memory
+cap_out.release()
+cv2.destroyAllWindows()
+
+
+
